2ec52b025268966a0f00ed29c0a4b2344d7bcc01
DESKTOP-C5CSPL2\dev030 <jcw9828@gmail.com>
2022-02-10 16:54:49 +0900

    프로모션 swiper 완료

--- js/main.js
+++ js/main.js
@@ -11,7 +11,62 @@ searchInputEl.addEventListener("focus", function () {
   searchInputEl.setAttribute("placeholder", "통합검색");
 });
 // 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused')
-    searchInputEl.setAttribute('placeholder', '')
+searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+const badgeEl = document.querySelector('header .badges')
+
+window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
+  if (window.scrollY > 500) {
+    // Badge 요소 숨기기!
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else {
+    // Badge 요소 보이기!
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300))
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소,지속시간,옵션)
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .5,
+    opacity: 1
   })
+})
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
+})
+
+new Swiper('.promotion .swiper-container', {
+  direction: 'horizontal', // 수평 슬라이드
+  slidesPerView: 3, // 보여줄 슬라이드 갯수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 위치
+  loop: true, // 반복 재생 여부
+  autoplay: {
+    delay: 2000
+  }, // 자동 재생 여부
+
+  
+  pagination: { // 페이지 번호 사용 여부
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
+  }
+})
