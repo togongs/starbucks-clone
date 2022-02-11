cda8fe426842811a19852b1e1bbae52183806a57
DESKTOP-C5CSPL2\dev030 <jcw9828@gmail.com>
2022-02-11 17:52:57 +0900

    youtube iframe api

---
+++ js/youtube.js
@@ -0,0 +1,27 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+const tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+function onYouTubeIframeAPIReady() {
+  new YT.Player("player", { //<div id="player"></div>
+    videoId: "WRrYdg8MAHA", // 최초 재생할 유튜브 영상 ID
+    playerVars: {
+        autoplay: true,
+        loop: true,
+        playlist: 'WRrYdg8MAHA' // 반복 재생할 유튜브 영상 ID 목록
+    },
+    evenets: {
+        onReady: function(event) {
+            event.target.mute() // 음소거
+        }
+    }
+  });
+}
+
+// An6LvWQuj_8
